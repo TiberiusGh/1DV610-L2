4ada2b2738f6ca683d7df488bcb5233c74103429
Tiberius Gherac <tiberius.gherac@gmail.com>
2025-09-10 22:02:32 +0200

    Add initial interface functions/public API for the module

---
+++ src/index.ts
@@ -0,0 +1,63 @@
+type ConsentCategories = {
+  essential: boolean
+  analytics: boolean
+  marketing: boolean
+}
+
+export function getConsents(): ConsentCategories | null {
+  const storedConsent = localStorage.getItem('consent-tracker')
+
+  if (!storedConsent) {
+    return null
+  }
+
+  try {
+    return JSON.parse(storedConsent)
+  } catch (error) {
+    return null
+  }
+}
+
+export function setConsents(consents: ConsentCategories): ConsentCategories {
+  localStorage.setItem('consent-tracker', JSON.stringify(consents))
+  return consents
+}
+
+export function uppdateConsent(category: string, value: boolean): void {
+  let storedConsent: ConsentCategories | null
+
+  try {
+    const stored = localStorage.getItem('consent-tracker')
+    storedConsent = stored ? JSON.parse(stored) : null
+  } catch (error) {
+    storedConsent = null
+  }
+
+  if (!storedConsent) {
+    storedConsent = {
+      essential: false,
+      analytics: false,
+      marketing: false
+    }
+  }
+
+  switch (category) {
+    case 'essential':
+      storedConsent.essential = value
+      break
+
+    case 'analytics':
+      storedConsent.analytics = value
+      break
+
+    case 'marketing':
+      storedConsent.marketing = value
+      break
+  }
+
+  localStorage.setItem('consent-tracker', JSON.stringify(storedConsent))
+}
+
+export function withdrawConsent() {
+  localStorage.removeItem('consent-tracker')
+}

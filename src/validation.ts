39d0a98efa4221533e017c44a84103aaf644f0e1
Tiberius Gherac <tiberius.gherac@gmail.com>
2025-09-11 19:32:12 +0200

    Add logic for when all categories are rejected

--- src/validation.ts
+++ src/validation.ts
@@ -1,7 +1,13 @@
+import type { ConsentCategories } from './types'
+
 export function validateDate(storedDate: Date): boolean {
   const currentDate = new Date()
   const expirationDate = new Date(storedDate)
   expirationDate.setMonth(storedDate.getMonth() + 12)
 
   return currentDate > expirationDate
+}
+
+export function validateFalseContents(consents: ConsentCategories) {
+  return !(consents.analytics || consents.essential || consents.marketing)
 }

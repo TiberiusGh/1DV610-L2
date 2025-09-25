fe7e3396a4bd7fb11faf58b359f6d4f2869ab658
Tiberius Gherac <tiberius.gherac@gmail.com>
2025-09-25 17:48:31 +0200

    Refactor ConsentTracker: update imports, streamline consent handling, and improve validation logic

--- src/ConsentTracker.ts
+++ src/ConsentTracker.ts
@@ -1,96 +1,86 @@
-import type { ConsentCategories } from './types'
+import type { ConsentCategories, ConsentsWithTimeStamp } from './types'
 import { LocalStorage } from './LocalStorage'
-import { CallbackManager } from './CallbackManager'
-import { ConsentsValidator } from './ConsentsValidator'
+import { Callback } from './Callback'
+import { Validator } from './Validator'
 
 export class ConsentTracker {
   #localStorage = new LocalStorage()
-  #callbackManager = new CallbackManager()
-  #consentsValidator = new ConsentsValidator()
+  #callback = new Callback()
+  #validator = new Validator()
 
+  // Wrapper function
   onConsentChange(callback: Function): ConsentTracker {
-    this.#callbackManager.onConsentChange(callback)
+    this.#callback.onConsentChange(callback)
     return this
   }
 
-  getConsents(): ConsentCategories {
+  // Wrapper function
+  getConsents(): ConsentsWithTimeStamp {
     return this.#localStorage.getConsents()
   }
 
   setConsents(consents: ConsentCategories): void {
-    const currentConsents = consents
-    currentConsents.uppdateTime = new Date()
-
-    const falseConsents =
-      this.#consentsValidator.validateFalseContents(currentConsents)
+    const falseConsents = this.#validator.validateFalseContents(consents)
 
     if (falseConsents) {
-      this.#localStorage.clearConsent()
-      this.#callbackManager.runCallback()
+      this.#handleInvalidConsents()
     } else {
-      this.#localStorage.saveConsent(currentConsents)
-      this.#callbackManager.runCallback(currentConsents)
+      this.#handleValidConsents(consents)
     }
   }
 
   uppdateConsent(category: string, value: boolean): void {
-    let storedConsent: ConsentCategories | null
+    const consents = this.#createConsentObject(category, value)
+    const falseConsents = this.#validator.validateFalseContents(consents)
 
-    try {
-      storedConsent = this.#localStorage.getConsents()
-    } catch (error) {
-      storedConsent = null
+    if (falseConsents) {
+      this.#handleInvalidConsents()
+    } else {
+      this.#handleValidConsents(consents)
     }
+  }
 
-    if (!storedConsent) {
-      storedConsent = {
-        essential: false,
-        analytics: false,
-        marketing: false,
-        uppdateTime: new Date()
-      }
+  #createConsentObject(category: string, value: boolean): ConsentCategories {
+    const consents = {
+      essential: false,
+      analytics: false,
+      marketing: false
     }
 
     switch (category) {
       case 'essential':
-        storedConsent.essential = value
+        consents.essential = value
         break
       case 'analytics':
-        storedConsent.analytics = value
+        consents.analytics = value
         break
       case 'marketing':
-        storedConsent.marketing = value
+        consents.marketing = value
         break
     }
-
-    storedConsent.uppdateTime = new Date()
-
-    const falseConsents =
-      this.#consentsValidator.validateFalseContents(storedConsent)
-
-    if (falseConsents) {
-      this.#localStorage.clearConsent()
-      this.#callbackManager.runCallback()
-    } else {
-      this.#localStorage.saveConsent(storedConsent)
-      this.#callbackManager.runCallback(storedConsent)
-    }
+    return consents
   }
 
   acceptAll(): void {
     const consentedAll = {
       essential: true,
       analytics: true,
-      marketing: true,
-      uppdateTime: new Date()
+      marketing: true
     }
-
-    this.#localStorage.saveConsent(consentedAll)
-    this.#callbackManager.runCallback(consentedAll)
+    this.#handleValidConsents(consentedAll)
   }
 
   declineAll(): void {
+    this.#handleInvalidConsents()
+  }
+
+  #handleValidConsents(consents: ConsentCategories) {
+    this.#localStorage.saveConsent(consents)
+    this.#callback.runCallback(consents)
+  }
+
+  #handleInvalidConsents() {
     this.#localStorage.clearConsent()
-    this.#callbackManager.runCallback()
+    this.#callback.runCallback()
   }
 }
